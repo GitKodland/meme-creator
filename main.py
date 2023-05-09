875f32a1d878093569c9fecf6b3b7033e774d581
GitKodland <125306926+GitKodland@users.noreply.github.com>
2023-05-09 13:01:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,30 +12,27 @@ def index():
         selected_image = request.form.get('image-selector')
 
         # Задание №2.Получаем текст
-        text_top = request.form.get('textTop')
-        text_bottom = request.form.get('textBottom')
+        
 
         # Задание №3. Получаем расположение текста
-        text_top_y = request.form.get('textTop_y')
-        text_bottom_y = request.form.get('textBottom_y')
+       
 
         # Задание №3. Получаем цвет текста
-        selected_color = request.form.get('color-selector')
+        
 
         return render_template('index.html', 
                                # отображаем выбранное изображение
                                selected_image=selected_image, 
 
                                # Задание №2. Отображаем текст
-                               text_bottom=text_bottom, 
-                               text_top=text_top,
+                               
 
                                # Задание №3. Отображаем цвет 
-                               selected_color=selected_color,
+                               
                                
                                #Задание №3. Отоброжаем расположение текста
-                               text_top_y=text_top_y,
-                               text_bottom_y=text_bottom_y)
+
+                               )
     else:
         # отображаем первое изображение по умолчанию
         return render_template('index.html', selected_image='logo.svg')
